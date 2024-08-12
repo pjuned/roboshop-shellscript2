662b379f8e3353f2a8f50e628ffc2cab428868fb
unknown <pjuned.it@gmail.com>
2024-08-13 00:11:04 +0530

    second script

---
+++ mongodb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo "Script started executing at $TIMESTAMP" &>> LOGFILE
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]
+then 
+    echo "$2  is $R failed $N"
+else
+    echo "$2  is $G success $N"
+fi
+
+}
+
+if [ $ID -ne 0 ]
+then
+ echo "you are not root user"
+ exit 1
+else
+    echo " you are root user"
+
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> LOGFILE
+
+VALIDATE $? "Copied Mongodb repo"
