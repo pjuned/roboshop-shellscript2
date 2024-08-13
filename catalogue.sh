129c59d4bdb5ca0b7cb932c910b3bb86c40a407f
unknown <pjuned.it@gmail.com>
2024-08-13 11:46:49 +0530

    edited

--- catalogue.sh
+++ catalogue.sh
@@ -9,13 +9,14 @@ mongodb_host=mongodb.devopsju.online
 
 TIMESTAMP=$(date +%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-echo "Script started executing at $TIMESTAMP" &>> LOGFILE
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
 
 if [ $1 -ne 0 ]
 then 
     echo -e "$2  is $R failed $N"
+    exit 1
 else
     echo -e "$2  is $G success $N"
 fi
@@ -31,7 +32,7 @@ else
     echo " you are root user"
 fi
 
-dnf module disable nodejs -y &>> LOGFILE
+dnf module disable nodejs -y &>> $LOGFILE
 
 VALIDATE $? "disable default nodejs"
 
@@ -39,13 +40,22 @@ dnf module enable nodejs:18 -y
 
 VALIDATE $? "enabling nodejs:18"
 
-dnf install nodejs -y &>> LOGFILE
+dnf install nodejs -y &>> $LOGFILE
 
 VALIDATE $? "Installing NOdejs"
 
-useradd roboshop &>> LOGFILE
+id roboshop
+if [$? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "Roboshop user already exists $Y skipping $N"
+fi
 
-mkdir /app
+useradd roboshop &>> $LOGFILE
+
+mkdir -p /app
 
 VALIDATE $? "creating App directory"
 
@@ -79,14 +89,14 @@ systemctl start catalogue
 
 VALIDATE $? "Starting catlaogue"
 
-cp /home/centos/roboshop-shellscript2/mongo.repo /etc/yum.repos.d/mongo.repo &>> LOGFILE
+cp /home/centos/roboshop-shellscript2/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copying Mongo repo"
 
-dnf install mongodb-org-shell -y &>> LOGFILE
+dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "installing mongodb client"
 
-mongo --host $mongodb_host </app/schema/catalogue.js
+mongo --host $mongodb_host </app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "loading catalogue data into mongodb"
--- mongodb.sh
+++ mongodb.sh
@@ -15,6 +15,7 @@ VALIDATE(){
 if [ $1 -ne 0 ]
 then 
     echo -e "$2  is $R failed $N"
+    exit 1
 else
     echo -e "$2  is $G success $N"
 fi
