e46f058e042165179722e89ba9bc9fd6408b517b
unknown <pjuned.it@gmail.com>
2025-08-07 10:32:34 +0530

    OK

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-AMI=ami-0b4f379183e5706b9 #this keeps on changing
+#AMI=ami-0b4f379183e5706b9 #this keeps on changing
+AMI=ami-09c813fb71547fc4f
 SG_ID=sg-0c1f012dbc6767c00 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z0453211BMHVK8GNH9ST
